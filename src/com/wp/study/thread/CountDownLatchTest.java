b75144646c9674a4dbd7662f60d69fe7c4658074
swiftwen <wenpeng0412@qq.com>
2018-04-18 09:53:47 +0800

    create thread test package

---
+++ src/com/wp/study/thread/CountDownLatchTest.java
@@ -0,0 +1,23 @@
+package com.wp.study.thread;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 
+ * @desc 
+ * @Author wenpeng
+ * @2018年4月18日 上午9:49:50
+ */
+public class CountDownLatchTest {
+
+	public static void main(String[] args) {
+		CountDownLatch countDownLatch = new CountDownLatch(3);
+		
+	}
+}
+class MyThread extends Thread{
+	@Override
+	public void run() {
+		
+	}
+}
